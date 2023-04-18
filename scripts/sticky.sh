fc6d54259749062e65a63d0f4a4e1f783ff6b602
sudoerr <121198893+sudoerr@users.noreply.github.com>
2023-04-18 15:21:27 -0500

    a

--- scripts/sticky.sh
+++ scripts/sticky.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+#if emailed
+
+#grab file
+
+#unzip .postit file
+
+#grab images
+
+#find color of sticky note
+
+#put paper to protect sticky
+
+#start printing with exact image
+
+#done
