908a8a242fa39465502670e451c49ad47cc9f2da
Eli <vmlinuz0hio@gmail.com>
2023-04-19 16:11:20 -0500

    OK

--- scripts/sticky.sh
+++ scripts/sticky.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-#if emailed
-
-#grab file
-
-#unzip .postit file
-
-#grab images
-
-#find color of sticky note
-
-#put paper to protect sticky
-
-#start printing with exact image
-
-#done
